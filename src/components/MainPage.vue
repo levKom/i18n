b43e65dafd67e48f117142245f5a7cf6c6bffa85
komd <dkomlev@teamworkretail.com>
2019-10-15 15:51:38 +0300

    test locale changes

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,21 +1,35 @@
 <template>
-<div>
-  <p>{{ $t('hello') }}</p>
-  <q-btn color="white" text-color="black" label="Standard" />
-  <button>LOA</button>
-</div>
+  <div>
+    <!-- <p>{{ $t('hello') }}</p> -->
+    <a href="#" @click="setLocale('en')">
+      <flag iso="us"></flag>
+    </a>
+    <a href="#" @click="setLocale('ru')">
+      <flag iso="ru"></flag>
+    </a>
+    <br />
+    <br />
+    <p>{{ $t('message') }}</p>
+    <p>
+      <i18n path="compText">
+        <br place="break" />
+        <a href="#" target="_blank" rel="noopener" place="action">{{ $t('compTextUrlName') }}</a>
+      </i18n>
+    </p>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'MainPage'
+  name: 'MainPage',
+
+  methods: {
+    setLocale(locale) {
+      this.$i18n.locale = locale
+    },
+  },
 }
 </script>
 
-<i18n>
-{
-  "en": {
-    "hello": "Hello i18n in SFC!"
-  }
-}
-</i18n>
+<style lang="scss">
+</style>
