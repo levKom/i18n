a099c9ab3d67b81c14ea377514e837ed2f84414b
komd <dkomlev@teamworkretail.com>
2019-10-15 12:11:04 +0300

    upd

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 import './plugins/quasar'
 import ModulesLoader from '@utils/modules-loader'
-import i18n from './i18n'
+import i18n from './plugins/i18n'
 
 Vue.config.productionTip = false
 
