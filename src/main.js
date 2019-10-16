f65090148fc57e427cd2a91b72c5cb5ca82686d6
komd <dkomlev@teamworkretail.com>
2019-10-16 14:09:17 +0300

    uninstalled bootstrap-vue, fixed styles

--- src/main.js
+++ src/main.js
@@ -6,14 +6,10 @@ import './plugins/quasar'
 import ModulesLoader from '@utils/modules-loader'
 import i18n from './plugins/i18n'
 import FlagIcon from 'vue-flag-icon'
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 
 Vue.config.productionTip = false
 Vue.use(FlagIcon)
-Vue.use(BootstrapVue)
 
 new Vue({
   router,
